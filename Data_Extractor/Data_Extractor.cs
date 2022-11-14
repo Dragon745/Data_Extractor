ec8a31beef092303de2745d051e0bdc45ea1f87e
Syed Qutubuddin Asadi <contact@syedqutubuddin.in>
2022-11-14 15:07:25 +0530

    v1.0.0.3.4

--- Data_Extractor/Data_Extractor.cs
+++ Data_Extractor/Data_Extractor.cs
@@ -12,18 +12,27 @@ namespace cAlgo.Robots
     [Robot(AccessRights = AccessRights.FullAccess)]
     public class Data_Extractor : Robot
     {
-        [Parameter(DefaultValue = "7")]
-        public int NoHistoryCandles { get; set; }
+        [Parameter(DefaultValue = "8")]
+        public int HistoryNeeded { get; set; }
+
+        [Parameter(DefaultValue = "150")]
+        public double StrandardGain { get; set; }
+
         [Parameter("Select Folder", DefaultValue = "D:\\Users\\conta\\Documents\\cTrader\\Data\\")]
         public string Folder { get; set; }
-        private ExponentialMovingAverage _ema;
+        private ExponentialMovingAverage _emaSlow;
+        private ExponentialMovingAverage _emaFast;
+        private RelativeStrengthIndex _rsi;
 
         protected override void OnStart()
         {
             // To learn more about cTrader Automate visit our Help Center:
             // https://help.ctrader.com/ctrader-automate
             Folder = Folder + "\\" + Symbol.Name + "\\";
-            _ema = Indicators.ExponentialMovingAverage(Bars.ClosePrices, 20);
+            _emaSlow = Indicators.ExponentialMovingAverage(Bars.ClosePrices, 50);
+            _emaFast = Indicators.ExponentialMovingAverage(Bars.ClosePrices, 5);
+            _rsi = Indicators.RelativeStrengthIndex(Bars.ClosePrices, 10);
+
         }
 
         protected override void OnTick()
@@ -33,14 +42,54 @@ namespace cAlgo.Robots
 
         protected override void OnBar()
         {
-            double[] data = new double[NoHistoryCandles];
-            int i = 1;
-            while (i < (NoHistoryCandles+1))
+            int NoHistoryCandles = HistoryNeeded;
+            //Create a List of HistoryCandles Height
+            List<double> Data = new List<double>();
+
+            
+            //Add Data to List {With Name of List Data} Not to Confuse with the Word Data.
+            NoHistoryCandles = NoHistoryCandles + 8;
+            while (NoHistoryCandles > 8)
             {
                 //Add data to array
-                data[(i-1)]     = Math.Round((Bars.ClosePrices.Last(i) - Bars.OpenPrices.Last(i)) / Symbol.PipValue, 1);
-                i++;
+                Data.Insert(0, Math.Round((Bars.ClosePrices.Last(NoHistoryCandles) - Bars.OpenPrices.Last(NoHistoryCandles)) / Symbol.PipValue, 1));
+                NoHistoryCandles--;
             }
+            
+            
+            //create a list with last 8 candles
+            List<double> HistoryMinMaxPrice = new List<double>();
+            while (NoHistoryCandles < 9 && NoHistoryCandles > 0)
+            {
+                HistoryMinMaxPrice.Add(Bars.HighPrices.Last(NoHistoryCandles));
+                HistoryMinMaxPrice.Add(Bars.LowPrices.Last(NoHistoryCandles));
+                NoHistoryCandles--;
+            }
+            //get the maximum value in list
+            double Max = HistoryMinMaxPrice.Max();
+            //get the minimum value in list
+            double Min = HistoryMinMaxPrice.Min();
+
+            Max = Math.Round((Max - Bars.ClosePrices.Last(9)) / Symbol.PipValue, 1);
+            Min = Math.Round((Min - Bars.ClosePrices.Last(9)) / Symbol.PipValue, 1);
+
+            
+            //Max is what percentage of StandardGain
+            double MaxPercentage = Math.Round(Max / StrandardGain, 2);
+            if (MaxPercentage > 1)
+            {
+                MaxPercentage = 1;
+            }
+
+            
+            //Min is what percentage of StandardGain
+            double MinPercentage = Math.Round(Min / StrandardGain, 2);
+            if (MinPercentage < -1)
+            {
+                MinPercentage = -1;
+            }
+
+
             //if path not present create it
             if (!System.IO.Directory.Exists(Folder))
             {
@@ -49,28 +98,50 @@ namespace cAlgo.Robots
             ////write data to CSV
             string FileNameX = Folder + "X.csv";
             string FileNameY = Folder + "Y.csv";
+            
+            //Write to Y
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(FileNameY, true))
             {
-                file.WriteLine(data[0]);
+                file.WriteLine(MinPercentage + "," + MaxPercentage);
                 file.Close();
             }
-            data[0] = Math.Round((Bars.OpenPrices.LastValue - _ema.Result.LastValue) / Symbol.PipValue, 1);
-   
-            i = data.Length - 1;
 
+            //Add Data from Indicators
+            Data.Insert(0, Math.Round(_rsi.Result.Last(9), 2));
+            Data.Insert(0, Math.Round((Bars.OpenPrices.Last(9) - _emaFast.Result.Last(9)) / Symbol.PipValue, 1));
+            Data.Insert(0, Math.Round((Bars.OpenPrices.Last(9) - _emaSlow.Result.Last(9)) / Symbol.PipValue, 1));
+            Data.Insert(0, Math.Round((_emaFast.Result.Last(9) - _emaSlow.Result.Last(9)) / Symbol.PipValue, 1));
+            //Print("RSI value: " + Math.Round(_rsi.Result.Last(9), 2));
+            //Print("EMA Fast value: " + Math.Round((Bars.OpenPrices.Last(9) - _emaFast.Result.Last(9)) / Symbol.PipValue, 1));
+            //Print("EMA Slow value: " + Math.Round((Bars.OpenPrices.Last(9) - _emaSlow.Result.Last(9)) / Symbol.PipValue, 1));
+            //Print("EMA Fast - EMA Slow value: " + Math.Round((_emaFast.Result.Last(9) - _emaSlow.Result.Last(9)) / Symbol.PipValue, 1));
+
+
+
+            // get lenght of list
+            int i = Data.Count - 1;
+
+            //Write to X
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(FileNameX, true))
             {
                 while (i > -1)
                 {
-                    Print("i From Inside the Loop: " + i);
-                    Print("data From Inside the Loop: " + data[i]);
-                    file.Write(data[i] + ",");
+                    if (i == 0)
+                    {
+                        file.Write(Data[i]);
+                    }
+                    else
+                    {
+                        file.Write(Data[i] + ",");
+                    }
                     i--;
                 }
                 file.WriteLine();
                 file.Close();
             }
 
+            //Stop the Robot
+            //Stop();
         }
 
         protected override void OnStop()
